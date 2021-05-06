862f96824ed82b730248e2b924afadb6bc5d0dd8
Iniyan Arul <iniyan.m@krazybee.com>
2021-05-06 23:58:32 +0530

    Dev test

--- app/src/main/java/com/iniyan/githubactionexample/MainActivity.kt
+++ app/src/main/java/com/iniyan/githubactionexample/MainActivity.kt
@@ -4,16 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import com.iniyan.githubactionexample.BuildConfig.baseUrl
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Log.e("tag",com.iniyan.githubactionexample.BuildConfig.baseUrl)
-        Log.e("tag",com.iniyan.githubactionexample.BuildConfig.gateway)
-        Log.e("tag",com.iniyan.githubactionexample.BuildConfig.apiUrl)
-        findViewById<TextView>(R.id.text).text = com.iniyan.githubactionexample.BuildConfig.baseUrl
+        findViewById<TextView>(R.id.text).text = baseUrl
 
     }
 }

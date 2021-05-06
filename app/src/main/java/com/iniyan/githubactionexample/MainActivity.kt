9c25e1dc26caee6b2a9f1fb94292efd5316db827
Iniyan Arul <iniyan.m@krazybee.com>
2021-05-06 23:26:46 +0530

    Dev test

--- app/src/main/java/com/iniyan/githubactionexample/MainActivity.kt
+++ app/src/main/java/com/iniyan/githubactionexample/MainActivity.kt
@@ -3,16 +3,15 @@ package com.iniyan.githubactionexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.firebase.BuildConfig
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.e("tag",com.iniyan.githubactionexample.BuildConfig.baseUrl)
-        Log.e("tag",com.iniyan.githubactionexample.BuildConfig.gateway)
-        Log.e("tag",com.iniyan.githubactionexample.BuildConfig.apiUrl)
+        Log.e("tag",BuildConfig.baseUrl)
+        Log.e("tag",BuildConfig.gateway)
+        Log.e("tag",BuildConfig.apiUrl)
 
     }
 }

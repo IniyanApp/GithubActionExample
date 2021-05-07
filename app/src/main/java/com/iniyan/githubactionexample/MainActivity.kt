2314f15fcb8cb991c04673f459bc21260f1801d7
Iniyan Arul <iniyan.m@krazybee.com>
2021-05-07 11:31:25 +0530

    Dev test Impl

--- app/src/main/java/com/iniyan/githubactionexample/MainActivity.kt
+++ app/src/main/java/com/iniyan/githubactionexample/MainActivity.kt
@@ -2,16 +2,13 @@ package com.iniyan.githubactionexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import com.iniyan.githubactionexample.BuildConfig.BASE_URL
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.text).text = BASE_URL
-        Log.e("tag","BaseURL $BASE_URL")
-
+//        findViewById<TextView>(R.id.text).text = BASE_URL
+//        Log.e("tag","BaseURL $BASE_URL")
     }
 }
